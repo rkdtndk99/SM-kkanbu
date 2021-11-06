ea3f3bb51d91e58dbe623df2adfae0f8878e7184
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-11-06 11:27:04 +0900

    'login'

---
+++ app/src/main/java/com/example/smswhapplication/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.smswhapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
