425cd70f69bf3accc50070c7a222ea2761d40d98
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-11-06 17:53:51 +0900

    login

--- app/src/main/java/com/example/smswhapplication/LoginActivity.java
+++ app/src/main/java/com/example/smswhapplication/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.smswhapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mFirebaseAuth; // firebase auth
-    private DatabaseReference mDatabaseRef; //realtime db
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private String userId;
 
     EditText login_email, login_pw;
     Button btn_login, btn_to_signup;
@@ -23,21 +31,34 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("SMSWH");
+
         login_email = findViewById(R.id.login_email);
-        login_pw = findViewById(R.id.login_email);
+        login_pw = findViewById(R.id.login_pw);
         btn_login = findViewById(R.id.btn_login);
         btn_to_signup = findViewById(R.id.btn_to_signup);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String strEmail = login_email.getText().toString();
-                String strPw = login_pw.getText().toString();
-
                 //이메일 db에 있는지 확인하는 코드 여기에
+                firebaseAuth.signInWithEmailAndPassword(login_email.getText().toString(), login_pw.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MatchingStartActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else{
+                                    Toast.makeText(LoginActivity.this, "로그인 실패" ,Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
-                Intent intent1 = new Intent(LoginActivity.this, MatchingStartActivity.class);
-                intent1.putExtra("email",strEmail);
+                Intent intent1 = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent1);
                 finish();
             }
--- app/src/main/java/com/example/smswhapplication/MatchingStartActivity.java
+++ app/src/main/java/com/example/smswhapplication/MatchingStartActivity.java
@@ -1,14 +1,36 @@
 package com.example.smswhapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MatchingStartActivity extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener itemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()){
+                case R.id.icon_kkanbu:
+                    //Intent intent = new Intent(MatchingStartActivity.this, MyKkanbuActivity.class);
+                    //startActivity(intent);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching_start);
+
+        @SuppressLint("ResourceType")
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(itemSelectedListener);
     }
 }
